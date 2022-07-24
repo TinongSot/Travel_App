41b626378bd6a936f109b30fbf3a45f5c5ac001a
Tinong Sot <tinong.sot@student.niptict.edu.kh>
2022-07-25 01:10:37 +0700

    new update

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:travel_app/misc/colors.dart';
+import 'package:travel_app/widgets/app_large_text.dart';
+import 'package:travel_app/widgets/app_text.dart';
+import 'package:travel_app/widgets/responsive_button.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -16,6 +18,8 @@ class _WelcomePageState extends State<WelcomePage> {
     "welcome-three.png",
   ];
 
+  List text = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +32,56 @@ class _WelcomePageState extends State<WelcomePage> {
                 height: double.maxFinite,
                 decoration: BoxDecoration(
                   image: DecorationImage(
-                      image: AssetImage("img/" + images[index])),
+                      image: AssetImage("img/" + images[index]),
+                      fit: BoxFit.cover),
+                ),
+                child: Container(
+                  margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          AppLargeText(text: "Trips"),
+                          AppText(
+                            text: "Mountain",
+                            size: 30,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            width: 250,
+                            child: AppText(
+                              text:
+                                  "Mountain hikes give you an incredible sence of freedom along with edurance test",
+                              color: AppColors.textColor2,
+                              size: 14,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 40,
+                          ),
+                          ResponsiveButton(
+                            width: 120,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: List.generate(3, (index) {
+                          return Container(
+                            width: 8,
+                            height: 25,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: AppColors.mainColor,
+                            ),
+                          );
+                        }),
+                      ),
+                    ],
+                  ),
                 ),
               );
             }));
